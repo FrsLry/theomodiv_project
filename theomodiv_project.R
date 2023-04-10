42e2fd8e4ada63616269c928024b139ad5c5ed60
FrsLry <francois.libert.leroy@gmail.com>
2023-04-10 10:06:17 +0200

    project final push

--- theomodiv_project.R
+++ theomodiv_project.R
@@ -44,7 +44,7 @@ model {
   # detec_log ~ dnorm(0, 0.1)
 }")
 ## parameters to save
-params <- c('e', 'c', 'psi', 'detec', 'x')
+params <- c('e', 'c', 'psi', 'detec', 'x', 'beta_day')
 ## hyperparameters
 na <- 1000 ; ni <- 1000 ; nt <- 5 ; nb <- 500 ; nc <- 1
 ## Generate initial values
@@ -69,6 +69,39 @@ data_jags <- list(obs = ifelse(data$C == 0, 0, 1),
                   time_day = data$time)
 
 ## Call JAGS
-out2 <- jagsUI::jags(data_jags, inits, params, "project/models/One_sp.txt", n.adapt = na, n.chains = nc,
+# out2 <- jagsUI::jags(data_jags, inits, params, "project/models/One_sp.txt", n.adapt = na, n.chains = nc,
+#                      n.thin = nt, n.iter = ni, n.burnin = nb, parallel = TRUE)
+# saveRDS(out2, "project/model_output/blue_jay_detection.rds")
+
+## Joint model
+data <- readRDS("project/data/jags_data_par.rds")
+
+obs <- array(dim = c(2,nrow(data_jags$obs), ncol(data_jags$obs)))
+data$`Blue Jay`$C <- ifelse((data$`Blue Jay`$C) ==0,0,1)
+data$`American Crow`$C <- ifelse(data$`American Crow`$C==0,0,1)
+obs[1,,] <- (data$`Blue Jay`$C)
+obs[2,,] <- (data$`American Crow`$C)
+
+## Second model with detection covariates
+data$`American Crow`$time <- data$`American Crow`$time %>%
+  as.data.frame() %>%
+  mutate(mean = round(rowMeans(.[,-1], na.rm = T), 2)) %>%
+  mutate(across(where(is.numeric), ~ coalesce(., mean))) %>%
+  select(-mean) %>% as.matrix()
+
+data_jags <- list(obs = obs,
+                  n_species = 2,
+                  n_site = nrow(data$`American Crow`$C),
+                  n_year = ncol(data$`American Crow`$C),
+                  time_day = (data$`American Crow`$time),
+                  # envt = dt_envt_scaled,
+                  switch = c(2,1))
+
+## parameters to save
+params <- c('e', 'c', 'psi', 'detec', 'x', 'beta_day', "mu_e", "gamma_e", "mu_c", "gamma_c", "psi_log")
+out3 <- jagsUI::jags(data_jags, inits, params, "project/models/Two_sp.txt", n.adapt = na, n.chains = nc,
                      n.thin = nt, n.iter = ni, n.burnin = nb, parallel = TRUE)
-saveRDS(out2, "project/model_output/blue_jay_detection.rds")
+# saveRDS(out3, "project/model_output/joint_model.rds")
+
+
+out2 <- readRDS("project/model_output/blue_jay_detection.rds")
